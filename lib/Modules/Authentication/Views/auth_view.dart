6880d23c5dc9d4f169ddb04765a4610a3b43003b
Avichal-Suneja <avichalarora2001@gmail.com>
2021-07-28 16:42:40 +0530

    AuthView

--- lib/Modules/Authentication/Views/auth_view.dart
+++ lib/Modules/Authentication/Views/auth_view.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:grocery_app/Modules/Authentication/Controllers/auth_controller.dart';
 class AuthView extends StatelessWidget {
   const AuthView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final controller = Get.find<AuthController>();
+    return controller.signIn.value? showSignIn() : showSignUp();
   }
+}
+
+Widget showSignIn(){
+  return Scaffold();
+}
+
+Widget showSignUp(){
+  return Scaffold();
 }
