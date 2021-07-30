5b332dfd158a5d5d7c5e28d6730306ba8bf7c9c5
Kunal Jaiswal <kunalmannat2001@gmail.com>
2021-07-31 03:59:20 +0530

    authview complete

--- lib/Modules/Authentication/Views/auth_view.dart
+++ lib/Modules/Authentication/Views/auth_view.dart
@@ -14,14 +14,17 @@ class AuthView extends StatelessWidget {
 
 Widget showSignIn(AuthController controller){
   return Scaffold(
-    // decoration: BoxDecoration(
-    //   image: DecorationImage(
-    //     image: Image.asset('background.jpg')
-    //   )
-    // ),
 
-    body:
-    Column(
+
+    body: Container(
+
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/background.jpg'),
+              fit: BoxFit.cover
+        )
+      ),
+    child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -31,48 +34,67 @@ Widget showSignIn(AuthController controller){
             style: TextStyle(
                 fontSize: 48,
                 color: Color(0xff039f00),
-                fontWeight: FontWeight.bold
+                fontWeight: FontWeight.bold,
+              fontFamily: 'PTSans',
+              letterSpacing: 1.5,
             ),
           ),
         ),
-        SizedBox(height: 40),
+        SizedBox(height: 32),
         Padding(
           padding: const EdgeInsets.fromLTRB(28.0, 0, 0, 0),
           child: Text('Email', style: TextStyle(
             color: Color(0xff039f00),
-              fontWeight: FontWeight.bold
+              fontWeight: FontWeight.bold,
+            fontSize: 16,
+            fontFamily: 'PTSans',
           ),),
         ),
-        SizedBox(height: 8),
+        SizedBox(height: 4),
         Center(
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(8),
             child: Container(
               color: Color(0xffe5e5e5),
               width: Get.width*0.9,
+              height: Get.height*0.07,
               child: TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 3.0)
+                  ),
+                ),
                 controller: controller.emailController,
               ),
             ),
           ),
         ),
-        SizedBox(height: 8),
+        SizedBox(height: 4),
         Padding(
           padding: const EdgeInsets.fromLTRB(28.0, 0, 0, 0),
           child: Text('Password',style: TextStyle(
             color: Color(0xff039f00),
-            fontWeight: FontWeight.bold
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+            fontFamily: 'PTSans',
+
           ),),
         ),
         SizedBox(height: 8),
         Center(
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(8),
             child: Container(
               color: Color(0xffe5e5e5),
               width: Get.width*0.9,
+              height: Get.height*0.07,
               child: TextField(
                 obscureText: true,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 3.0)
+                  ),
+                ),
                 controller: controller.passwordController,
               ),
             ),
@@ -94,7 +116,8 @@ Widget showSignIn(AuthController controller){
               ),
               child: Text('Sign In', style: TextStyle(
                 fontSize: 28,
-                color: Color(0xff039f00)
+                color: Color(0xff039f00),
+                fontFamily: 'PTSans',
               ),),
             ),
           ),
@@ -107,22 +130,189 @@ Widget showSignIn(AuthController controller){
             children: [
               Text('New User', style: TextStyle(
                   color: Colors.black,
-                  fontSize: 16
+                  fontSize: 18,
+                fontFamily: 'PTSans',
           )),
-              TextButton(onPressed: controller.showSignUp,
+              TextButton(
                 child: Text('Sign Up', style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 16,
-                    fontWeight: FontWeight.bold
-                ))
+                  color: Color(0xff039f00),
+                  fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  fontFamily: 'PTSans',
+                )),
+                onPressed: controller.showSignUp,
               ),
             ],
           ),
       ],
     ),
+    ),
   );
 }
 
 Widget showSignUp(AuthController controller){
-  return Container();
+  return Scaffold(
+    body: Container(
+
+      decoration: BoxDecoration(
+      image: DecorationImage(
+      image: AssetImage('assets/background.jpg'),
+  fit: BoxFit.cover
+  )
+  ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+    Center(
+    child: Text(
+    'SIGN UP',
+      style: TextStyle(
+          fontSize: 48,
+          color: Color(0xff039f00),
+          fontWeight: FontWeight.bold,
+        fontFamily: 'PTSans',
+        letterSpacing: 1.5,
+      ),
+    ),
+  ),
+        SizedBox(height: 16),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(28.0, 0, 0, 0),
+          child: Text('Name', style: TextStyle(
+              color: Color(0xff039f00),
+              fontWeight: FontWeight.bold,
+            fontSize: 16,
+            fontFamily: 'PTSans',
+          ),),
+        ),
+        SizedBox(height: 4),
+        Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Container(
+              color: Color(0xffe5e5e5),
+              width: Get.width*0.9,
+              child: TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 3.0)
+                  ),
+                ),
+                controller: controller.nameController,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 4),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(28.0, 0, 0, 0),
+          child: Text('Phone Number', style: TextStyle(
+              color: Color(0xff039f00),
+              fontWeight: FontWeight.bold,
+            fontSize: 16,
+            fontFamily: 'PTSans',
+          ),),
+        ),
+        SizedBox(height: 4),
+        Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Container(
+              color: Color(0xffe5e5e5),
+              width: Get.width*0.9,
+              child: TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 3.0)
+                  ),
+                ),
+                controller: controller.phoneController,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 4),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(28.0, 0, 0, 0),
+          child: Text('Email', style: TextStyle(
+              color: Color(0xff039f00),
+              fontWeight: FontWeight.bold,
+            fontSize: 16,
+            fontFamily: 'PTSans',
+          ),),
+        ),
+        SizedBox(height: 4),
+        Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Container(
+              color: Color(0xffe5e5e5),
+              width: Get.width*0.9,
+              child: TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 3.0)
+                  ),
+                ),
+                controller: controller.emailController,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 4),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(28.0, 0, 0, 0),
+          child: Text('Password',style: TextStyle(
+              color: Color(0xff039f00),
+              fontWeight: FontWeight.bold,
+            fontSize: 16,
+            fontFamily: 'PTSans',
+          ),),
+        ),
+        SizedBox(height: 8),
+        Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Container(
+              color: Color(0xffe5e5e5),
+              width: Get.width*0.9,
+              child: TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 3.0)
+                  ),
+                ),
+                obscureText: true,
+                controller: controller.passwordController,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 16),
+        Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: SizedBox(
+              height: 46,
+              width: 147,
+              child: ElevatedButton(
+                onPressed: controller.signIn,
+                style: ElevatedButton.styleFrom(
+                    primary: Color(0xffe5e5e5),
+                    shadowColor: Colors.black12,
+                    elevation: 5
+                ),
+                child: Text('Register', style: TextStyle(
+                    fontSize: 28,
+                    color: Color(0xff039f00),
+                  fontFamily: 'PTSans',
+                ),),
+              ),
+            ),
+          ),
+        ),
+  ]),
+    ),
+  );
 }
--- lib/Utils/appTheme.dart
+++ lib/Utils/appTheme.dart
@@ -4,4 +4,5 @@ final lightTheme = ThemeData(
   visualDensity: VisualDensity.adaptivePlatformDensity,
   primaryColor: Color(0xFFFFFFFF),
   backgroundColor: Color(0xFFFFFFFF),
+  fontFamily: 'PTSans'
 );
